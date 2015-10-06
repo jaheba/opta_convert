f09837d5b42c6efeac0ba99551d87e2acd5b7da4
Jasper Schulz <jasper.b.schulz@googlemail.com>
2015-10-06 15:45:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Opta Convert',
-    py_modules=['opta_convert'],
+    packages=['opta_convert'],
     entry_points={
         'console_scripts': ['opta_convert=opta_convert:main'],},
     install_reuires=[
